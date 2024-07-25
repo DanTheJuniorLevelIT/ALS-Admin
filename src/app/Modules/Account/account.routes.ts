3743e0d13b9cc20f96435a5aa7627f0c074d6f42
CrazyRAy <curamengray3@gmail.com>
2024-07-25 20:10:38 +0800

    updated ROUTes

--- src/app/Modules/Account/account.routes.ts
+++ src/app/Modules/Account/account.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AccountInfoComponent } from './account-info/account-info.component';
+import { EditComponent } from './edit/edit.component';
 
-export const accountRoute: Routes = [];
+export const accountRoute: Routes = [
+    {path: 'accountInfo', component: AccountInfoComponent},
+    {path: 'edit', component: EditComponent},
+    {path: '', redirectTo: 'accountInfo', pathMatch: 'full'}
+];
