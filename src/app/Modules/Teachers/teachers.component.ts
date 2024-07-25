e2d2a3337cfe04938b29995a910613acfce4ea8c
CrazyRAy <curamengray3@gmail.com>
2024-07-25 18:43:21 +0800

    update Componets

---
+++ src/app/Modules/Teachers/teachers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teachers',
+  standalone: true,
+  imports: [],
+  templateUrl: './teachers.component.html',
+  styleUrl: './teachers.component.css'
+})
+export class TeachersComponent {
+
+}
