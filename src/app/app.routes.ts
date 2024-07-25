cf01f34c019487a1da8a7d1bed2dec74e5a0265c
Dan Malayo <danielmalayo2@gmail.com>
2024-07-25 13:39:01 +0800

    Updated Lazy Loading and Router Outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import { homeRoute } from './Modules/Home/home.routes';
+import { accountRoute } from './Modules/Account/account.routes';
+import { studentRoute } from './Modules/Students/student.routes';
+import { classRoute } from './Modules/Classes/class.routes';
+import { teacherRoute } from './Modules/Teachers/teacher.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'main', component: MainComponent,
+        children: [
+            {
+                path: 'Home',
+                loadChildren: () => import('./Modules/Home/home.routes').then(r=>homeRoute)
+            },
+            {
+                path: 'Class',
+                loadChildren: () => import('./Modules/Classes/class.routes').then(r=>classRoute)
+            },
+            {
+                path: 'Student',
+                loadChildren: () => import('./Modules/Students/student.routes').then(r=>studentRoute)
+            },
+            {
+                path: 'Teacher',
+                loadChildren: () => import('./Modules/Teachers/teacher.routes').then(r=>teacherRoute)
+            },
+            {
+                path: 'Account',
+                loadChildren: () => import('./Modules/Account/account.routes').then(r=>accountRoute)
+            },
+            {path: '', redirectTo: 'Home', pathMatch: 'full'}
+        ]
+    },
+    {path: '', redirectTo: 'login', pathMatch: 'full'}
+];
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+export class MainComponent {
+
+}
