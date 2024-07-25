9af7af117f4ff4fd8899deda4aaf24abe7343d64
CrazyRAy <curamengray3@gmail.com>
2024-07-25 20:51:41 +0800

    udated

--- src/app/Modules/Home/home.routes.ts
+++ src/app/Modules/Home/home.routes.ts
@@ -2,9 +2,16 @@ import { Routes } from '@angular/router';
 
 import { EnrollStudentsComponent } from './enroll-students/enroll-students.component';
 import { AssignClassComponent } from './assign-class/assign-class.component';
+import { MainHomeComponent } from './main-home/main-home.component';
 
 export const homeRoute: Routes = [
-    {path: 'enroll', component: EnrollStudentsComponent},
-    {path: 'assingclass', component: AssignClassComponent},
-    {path: '', redirectTo: 'enroll', pathMatch: 'full'}
+    {path: 'mainHome', component: MainHomeComponent,
+        children:[
+            {path: 'enroll', component: EnrollStudentsComponent},
+            {path: 'assingclass', component: AssignClassComponent},
+            {path: '', redirectTo: 'enroll', pathMatch: 'full'}
+        ]
+    },
+    {path: '', redirectTo: 'mainHome', pathMatch: 'full'}
+   
 ];
--- src/app/Modules/Students/student.routes.ts
+++ src/app/Modules/Students/student.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { ViewStudentsComponent } from './view-students/view-students.component';
 import { UpdaterecordStudentComponent } from './updaterecord-student/updaterecord-student.component';
+import { MainStudentsComponent } from './main-students/main-students.component';
 
 export const studentRoute: Routes = [
-    {path: 'viewStudent', component: ViewStudentsComponent},
-    {path: 'updateRecord', component: UpdaterecordStudentComponent},
-    {path: '', redirectTo: 'viewStudent', pathMatch: 'full'}
+    {path: 'mainStudent', component: MainStudentsComponent,
+        children:[
+            {path: 'viewStudent', component: ViewStudentsComponent},
+            {path: 'updateRecordS', component: UpdaterecordStudentComponent},
+            {path: '', redirectTo: 'viewStudent', pathMatch: 'full'}
+        ]
+    },
+    {path: '', redirectTo: 'mainStudent', pathMatch: 'full'}
+   
 ];
