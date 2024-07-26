d4eb9817a45d81ab552ae614fcc8ed3d66de3253
elzaina <naborelzainacarbonel@gmail.com>
2024-07-26 11:59:53 +0800

    update

--- src/app/Modules/Home/enroll-students/enroll-students.component.ts
+++ src/app/Modules/Home/enroll-students/enroll-students.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-enroll-students',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './enroll-students.component.html',
   styleUrl: './enroll-students.component.css'
 })
