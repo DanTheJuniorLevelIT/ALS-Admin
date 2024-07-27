f47ba20161cf595ec56ee9052c625d1e99e2d559
CrazyRAy <curamengray3@gmail.com>
2024-07-27 15:07:35 +0800

    update

--- src/app/Modules/Students/view-students/view-students.component.ts
+++ src/app/Modules/Students/view-students/view-students.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-view-students',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './view-students.component.html',
   styleUrl: './view-students.component.css'
 })
