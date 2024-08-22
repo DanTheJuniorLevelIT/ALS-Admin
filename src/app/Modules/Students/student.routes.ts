0539931038cc531753cc2a0ee7bcbb2d1beabad4
elzaina <naborelzainacarbonel@gmail.com>
2024-08-22 11:25:21 +0800

    update

--- src/app/Modules/Students/student.routes.ts
+++ src/app/Modules/Students/student.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { ViewStudentsComponent } from './view-students/view-students.component';
 import { UpdaterecordStudentComponent } from './updaterecord-student/updaterecord-student.component';
 import { MainStudentsComponent } from './main-students/main-students.component';
+import { ViewDetailsComponent } from './view-details/view-details.component';
 
 export const studentRoute: Routes = [
     {path: 'mainStudent', component: MainStudentsComponent,
         children:[
             {path: 'viewStudent', component: ViewStudentsComponent},
             {path: 'updateRecordS', component: UpdaterecordStudentComponent},
+            {path: 'viewDetails', component: ViewDetailsComponent},
             {path: '', redirectTo: 'viewStudent', pathMatch: 'full'}
         ]
     },
---
+++ src/app/Modules/Students/view-details/view-details.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-view-details',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './view-details.component.html',
+  styleUrl: './view-details.component.css'
+})
+export class ViewDetailsComponent {
+
+}
